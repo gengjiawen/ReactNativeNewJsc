53b6c1b04d4159272c8334b85bf39db94a9993e4
gengjiawen <technicalcute@gmail.com>
2018-11-28 23:04:12 +0800

    temp fix for android part

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,14 @@
 module.exports = function (api) {
-  const platform = api.caller(caller => caller.platform);
+  // api.cache(true)
+  // const platform = api.caller(caller => caller.platform);
 
   if (platform === 'android') {
     return {
       presets: [],
       plugins: ["@babel/plugin-transform-react-jsx",
         "@babel/plugin-proposal-class-properties",
+        "@babel/plugin-proposal-nullish-coalescing-operator",
+        "@babel/plugin-proposal-optional-chaining",
         "@babel/plugin-transform-flow-strip-types",
         "@babel/plugin-transform-modules-commonjs"]
     }
